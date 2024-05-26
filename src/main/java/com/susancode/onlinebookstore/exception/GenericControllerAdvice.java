674fe1e9d9792ced51b9cdd4614f14bebbf2b2cb
DevSuzzy <egeonususan6@gmail.com>
2024-05-26 23:39:47 +0100

    exception fixes

--- src/main/java/com/susancode/onlinebookstore/exception/GenericControllerAdvice.java
+++ src/main/java/com/susancode/onlinebookstore/exception/GenericControllerAdvice.java
@@ -4,11 +4,17 @@ import com.susancode.onlinebookstore.dto.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 @Slf4j
@@ -23,5 +29,28 @@ public class GenericControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { BadRequestException.class, InterruptedException.class })
     protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex , new ApiResponse(false,  ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = { UnAuthorized.class })
+    protected ResponseEntity<Object> handleUnAuthorized(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex , new ApiResponse(false,  ex.getMessage()), new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex , new ApiResponse(false,  ex.getMessage()), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
+
+        Optional<FieldError> error = bindingResult.getFieldErrors().stream().findFirst();
+        String message = "bad request";
+        if (error.isPresent()) {
+            message = error.get().getDefaultMessage();
+        }
+
+        return new ResponseEntity<>(new ApiResponse<>(false, message), HttpStatus.BAD_REQUEST);
     }
 }
